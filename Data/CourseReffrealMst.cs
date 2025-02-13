2b6e43bef2912b847aa95e31c4a54d432fa7b915
NayanMachhi007 <nayankumarmachhi05@gmail.com>
2025-02-13 15:35:27 +0530

    New Code Updated By Nayan

---
+++ Data/CourseReffrealMst.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace The_One_Web_Technology.Data
+{
+    public class CourseReffrealMst
+    {
+        [Key]
+        public int id { get; set; }
+        public int creffrealpoint { get; set; }
+
+        [ForeignKey("courseDetailsMst")]
+        public int courseid { get; set; }
+        public courseDetailsMst courseDetailsMst { get; set; }
+
+        public DateTime startingdate { get; set; } = System.DateTime.Now;
+        public DateTime? endingdate { get; set; }
+        public Boolean status { get; set; }
+    }
+}
